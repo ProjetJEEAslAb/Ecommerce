76c5f2e3b7933a89292faa124739a3c6f2971883
inti-0265 <inti-0265@192.168.1.170>
2017-10-25 12:15:40 +0200

    creation : classe client

--- 10_ProjetEcommerce/src/fr/adaming/model/Client.java
+++ 10_ProjetEcommerce/src/fr/adaming/model/Client.java
@@ -1,5 +1,108 @@
 package fr.adaming.model;
 
-public class Client {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="clients")
+public class Client implements Serializable {
+
+	// declaration des attributs
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_client")
+	private long id;
+	private String nom;
+	private String Client;
+	private String mail;
+	private String tel;
+
+//=======================================================================//
+	
+	// declarations des constructeurs
+
+	public Client() {
+		super();
+	}
+
+	// constructeur sans id
+	public Client(String nom, String client, String mail, String tel) {
+		super();
+		this.nom = nom;
+		Client = client;
+		this.mail = mail;
+		this.tel = tel;
+	}
+
+	// constructeur avec id
+
+	public Client(long id, String nom, String client, String mail, String tel) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		Client = client;
+		this.mail = mail;
+		this.tel = tel;
+	}
+	
+//=======================================================================//
+
+	// declaration des getters et setters
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getClient() {
+		return Client;
+	}
+
+	public void setClient(String client) {
+		Client = client;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+
+//=======================================================================//
+	//methode toString
+	
+	@Override
+	public String toString() {
+		return "Client [id=" + id + ", nom=" + nom + ", Client=" + Client + ", mail=" + mail + ", tel=" + tel + "]";
+	}
 
 }

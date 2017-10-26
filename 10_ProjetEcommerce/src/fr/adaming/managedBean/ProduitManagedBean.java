dabed9fe7ec6c4c98038ac56524356c740839718
inti-0265 <inti-0265@192.168.1.170>
2017-10-26 09:37:46 +0200

    anissa : produit managedbean

---
+++ 10_ProjetEcommerce/src/fr/adaming/managedBean/ProduitManagedBean.java
@@ -0,0 +1,20 @@
+package fr.adaming.managedBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name="cMB")
+@ViewScoped
+public class ProduitManagedBean {
+
+	
+//=======================================================================//
+	//les methodes
+	
+	
+	
+	
+}
